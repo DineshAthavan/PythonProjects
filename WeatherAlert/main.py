66d1db040d7156a2b3ed0cd646124bb62f08f917
Dinesh Athavan (MS/ESG2-VM) <Athavan.Dinesh@in.bosch.com>
2026-04-24 18:29:27 +0530

    Weather alert app updated with sms function

--- WeatherAlert/main.py
+++ WeatherAlert/main.py
@@ -1,16 +1,32 @@
 import requests
 import os
+from twilio.rest import Client
 
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = os.environ.get(OWM_API_KEY)
+account_sid = os.environ.get(TWILIO_SID)
+auth_token = os.environ.get(TWILIO_AUTH_TOKEN)
 lat = 11.016844
 lon = 76.955833
 
 params = {
     "lat": lat,
     "lon": lon,
+    "cnt": 4,
     "appid": api_key,
 }
 
 response = requests.get(OWM_ENDPOINT, params=params)
-print(response.status_code)
+response.raise_for_status() #exception handling
+weather_data = response.json()
+
+x1 = weather_data["list"][0]["weather"][0]["main"]
+x2 = weather_data["list"][0]["weather"][0]["description"]
+msg_content = f"{x1}\n{x2}"
+
+client = Client(account_sid, auth_token)
+message = client.messages.create(
+    body=msg_content,
+    from_=os.environ.get(FROM_NUMBER),
+    to="<entermyphno>",
+)
